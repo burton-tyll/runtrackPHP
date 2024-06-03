0a3ce80efe7eb4f073fa0af1e321a8623ed1cfba
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-03 10:15:07 +0200

    job01

---
+++ jour01/index.php
@@ -0,0 +1,30 @@
+<?php
+//Afficher Vive La Plateforme
+$text = 'LaPlateforme';
+
+$vive = 'Vive ';
+
+$exclamation = '!';
+
+echo($vive.$text.$exclamation);
+
+//Afficher 6
+$chiffre = 6;
+
+echo($chiffre);
+
+//Afficher 6 + 4
+
+$chiffre = $chiffre+4;
+
+echo($chiffre);
+
+//Afficher true
+
+$true = true;
+echo($true);
+
+//Afficher false
+$false = false;
+echo($false);
+?>
